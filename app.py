7a9c693f3cfe00633c3dd430060a75edfed279e1
Ethan Cavill <ethancavill@gmail.com>
2022-02-19 11:59:28 +1030

    Add a flask-wtf form

--- app.py
+++ app.py
@@ -1,16 +1,35 @@
-from flask import Flask
+from flask import Flask, redirect, render_template, request, session, url_for
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "blahblahblah"
+
+
+class NameForm(FlaskForm):
+    name = StringField("What is your name?", validators=[DataRequired()])
+    submit = SubmitField("Submit")
 
 
 def index():
-    return "<h1>Hello World!</h1>"
+    form = NameForm()
+    if form.validate_on_submit():
+        session["name"] = form.name.data
+        return redirect(url_for("index"))
+    return render_template("index.html", form=form, name=session.get("name"))
 
 
-def user(name):
-    return "<h1>Hello, {}!</h1>".format(name)
+def page_not_found(e):
+    return "<h1>Page not found!</h1>", 404
 
 
-app.add_url_rule("/", "index", index)
+def internal_server_error(e):
+    return "<h1>Bad request!</h1>", 500
 
-app.add_url_rule("/user/<name>", "user", user)
+
+app.add_url_rule("/", view_func=index, methods=["GET", "POST"])
+
+app.register_error_handler(404, page_not_found)
+
+app.register_error_handler(500, internal_server_error)
